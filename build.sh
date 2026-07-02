f9cd3febeb90fe610794e953e3102a130dffa899
naamfung <jorsion@hotmail.com>
2026-07-02 10:00:54 +0800

    v0.2.4

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 rm -rf *.exe*
-rm -rf *.exe
 # CC='zig cc' go build -ldflags '-H windowsgui -extldflags "-Wl,--subsystem,windows"'
 go build -ldflags '-H windowsgui -extldflags "-Wl,--subsystem,windows"'
